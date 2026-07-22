9764566bb699c8e3fc70576daf0530679aca7624
truongquoc <quoc93346@gmail.com>
2026-07-22 20:24:50 +0700

    Update AppRoute.js

--- backend/AppRoute.js
+++ backend/AppRoute.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import * as HotelsController from './controllers/HotelsController.js'
 import * as UsersController from './controllers/UsersController.js'
+import * as DestinationsController from './controllers/DestinationsController.js'
+import * as DestinationImageController from './controllers/DestinationImageController.js'
 import insertHotelRequests from './dtos/requests/hotel/insertHotelRequests.js'
 import updateHotelRequests from './dtos/requests/hotel/updateHotelRequests.js'
 import insertUserRequests from './dtos/requests/user/insertUserRequests.js'
 import updateUserRequests from './dtos/requests/user/updateUserRequests.js'
+import insertDestinationRequests from './dtos/requests/destination/insertDestinationRequests.js'
+import updateDestinationRequests from './dtos/requests/destination/updateDestinationRequests.js'
+
 import asyncHandler from './middlewares/asyncHandler.js'
 import validate from './middlewares/validate.js'
+import imageUpload from './middlewares/imageUpload.js'
 
 const router = express.Router()
 
@@ -15,17 +21,22 @@ export function AppRoute(app){
     router.get('/hotels', asyncHandler(HotelsController.getHotels))
     router.get('/hotels/:id', asyncHandler(HotelsController.getHotelById))
     router.post('/hotels', validate(insertHotelRequests), asyncHandler(HotelsController.insertHotel))
-    router.put('/hotels', validate(updateHotelRequests), asyncHandler(HotelsController.updateHotel))
+    router.put('/hotels/:id', validate(updateHotelRequests), asyncHandler(HotelsController.updateHotel))
     router.delete('/hotels/:id', asyncHandler(HotelsController.deleteHotel))
 
     router.get('/users', asyncHandler(UsersController.getUsers))
     router.get('/users/:id', asyncHandler(UsersController.getUserById))
     router.post('/users', validate(insertUserRequests), asyncHandler(UsersController.insertUser))
-    router.put('/users', validate(updateUserRequests), asyncHandler(UsersController.updateUser))
+    router.put('/users/:id', validate(updateUserRequests), asyncHandler(UsersController.updateUser))
     router.delete('/users/:id', asyncHandler(UsersController.deleteUser))
     router.post('/users/login', asyncHandler(UsersController.loginUser))
 
-
+    router.get('/destinations', asyncHandler(DestinationsController.getDestinations))
+    router.get('/destinations/:id', asyncHandler(DestinationsController.getDestinationById))
+    router.post('/destinations', validate(insertDestinationRequests), asyncHandler(DestinationsController.insertDestination))
+    router.put('/destinations/:id', validate(updateDestinationRequests), asyncHandler(DestinationsController.updateDestination))
+    router.delete('/destinations/:id', asyncHandler(DestinationsController.deleteDestination))
+    router.post('/destinations/:destinationId/images', imageUpload.array('images'), asyncHandler(DestinationImageController.uploadImages))
 
     app.use('/api/', router)
 }
