e0b3beb20e1e0474540bd2dd19adfcf7e8975d6b
truongquoc <quoc93346@gmail.com>
2026-07-22 23:17:52 +0700

    Update imageUpload.js

--- backend/middlewares/imageUpload.js
+++ backend/middlewares/imageUpload.js
@@ -1,29 +1,37 @@
 import path from 'path'
+import fs from 'fs'
 import multer from 'multer'
-const storage = multer.diskStorage({
-    destination: function(req, res, callback) {
-        const destinationPath = path.join(__dirname, '../uploads/')
-        callback(null, destinationPath)
-    },
-    filename: function(req, file, callback) {
-        const uniqueName = `${Date.now()}-${file.originalname}`
-        callback(null, uniqueName)
-    },
-})
 
 const fileFilter = (req, file, callback) => {
-    if(file.mimetype.startsWith('image')){
+    if (file.mimetype.startsWith('image')) {
         callback(null, true);
     } else {
         callback(new Error('Chỉ được phép tải file ảnh!'), false);
     }
 }
 
-const upload = multer({
-    storage,
-    fileFilter, 
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    }
-})
-export default upload
+function createUploader(folderName) {
+    const storage = multer.diskStorage({
+        destination: function (req, res, callback) {
+            const destinationPath = path.join(__dirname, `../uploads/${folderName}/`)
+            if (!fs.existsSync(destinationPath)) {
+                fs.mkdirSync(destinationPath, { recursive: true })
+            }
+            callback(null, destinationPath)
+        },
+        filename: function (req, file, callback) {
+            const uniqueName = `${Date.now()}-${file.originalname}`
+            callback(null, uniqueName)
+        },
+    })
+
+    return multer({
+        storage,
+        fileFilter,
+        limits: { fileSize: 1024 * 1024 * 5 }
+    })
+}
+
+export const destinationImageUpload = createUploader('destinations')
+export const hotelImageUpload = createUploader('hotels')
+export const roomImageUpload = createUploader('rooms')
