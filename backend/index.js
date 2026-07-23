c4de42d26e22dd067f52ea0c3321f58a8cbc60fe
truongquoc <quoc93346@gmail.com>
2026-07-23 16:39:18 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config() 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    credentials: true
+}))
+
 app.use(express.json())
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({extended: true}))
+
+// Serve ảnh tĩnh
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 import { AppRoute } from './AppRoute.js'
 app.get('/', (req, res) => {})
